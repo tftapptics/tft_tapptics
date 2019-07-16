d1a9db3858a5215ff2ffcd08a9901231c87c7895
raechel <odom.raechel@gmail.com>
2019-07-16 11:54:26 -0600

    enzyme set up and working properly

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,5 @@
-import * as enzyme from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
-// import { configure } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 
 configure({ adapter: new Adapter() });
