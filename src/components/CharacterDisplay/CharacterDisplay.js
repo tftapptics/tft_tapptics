140a09b7e7b1f9b727ef8ba458d4bec60caf308a
Duy Vu <duyv.thanh@gmail.com>
2019-07-17 13:56:31 -0600

    Add details to characterdisplay

--- src/components/CharacterDisplay/CharacterDisplay.js
+++ src/components/CharacterDisplay/CharacterDisplay.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import SynergyIcons from '../SyngeryIcons/SyngeryIcons'
 
 const CharacterDisplay = () => {
   return (
     <div>
-      <h4>Name</h4>
-      <div className='champion-stats'>
-        <p className='health'>Health:</p>
-        <p className='mana'>Mana:</p>
-        <p className='dmg'>Damage:</p>
-        <p className='atk-speed'>Attack-Speed:</p>
+      <div className='champion-img-area'>
+        <h4 className='name'>Name</h4>
+        <img className='champ-img' src='#' alt='Image of Champion' />
       </div>
+      <div className='champion-stats'>
+        <p>Stats</p>
+        <p className='health stat'>Health:</p>
+        <p className='mana stat'>Mana:</p>
+        <p className='dmg stat'>Damage:</p>
+        <p className='atk-speed stat'>Attack-Speed:</p>
+      </div>
+      <div className='synergies-area'>
+        <SynergyIcons />
+      </div>
+      <button className='back'>Back Button</button>
     </div>
   )
 }
---
+++ src/components/SyngeryIcons/SyngeryIcons.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SyngeryIcons = () => {
+  return (
+    <div className='syn-block'>
+      <img className='syn-img' src='#' alt='Icon of Synergy' />
+      <p className='syn-description'>This is about the synergy</p>
+    </div>
+  )
+}
+
+export default SyngeryIcons;
