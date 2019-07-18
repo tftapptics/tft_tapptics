338934b43f32d271d9a24907d0e64c1c762c93f6
raechel <odom.raechel@gmail.com>
2019-07-18 14:00:25 -0600

    404 page styles add and ready to go

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import sadMumu from '../../assets/images/sad_mumu.png';
+import sadMumu from '../../assets/images/404_amumu.png';
 
 export default function NoMatch() {
   return (
-    <div>
+    <div className="NoMatch">
       <img src={sadMumu} alt="amumu, the sad mummuy. sorry, this page does not exist" />
+     <div>
       <h2>Sorry, that page does not exist!</h2>
-      <Link to="/">
-        <button>
-          Back
-        </button>
-      </Link>
+        <Link to="/">
+          <button>
+            Back
+          </button>
+        </Link>
+     </div>
     </div>
   )
 }
