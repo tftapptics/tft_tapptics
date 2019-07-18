6f526be6ceba96a183ad908eaa89264821efa38e
raechel <odom.raechel@gmail.com>
2019-07-18 11:00:57 -0600

    styles to focus outline on burger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,6 +10,7 @@ export default function Header() {
           <a id="about" className="menu-item" href="/items">Items</a>
           <a id="contact" className="menu-item" href="/Champions">Champions</a>
           <a id="contact" className="menu-item" href="/Synergies">Synergies</a>
+          <a id="contact" className="menu-item" href="/Carousel">Carousel</a>
       </Menu>
       <img src={logo} alt="T.F. Tapptics" />
     </div>
