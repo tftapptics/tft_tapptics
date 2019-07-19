28a2a7453e3f1788a85e425b352f542fe1637a23
Duy Vu <duyv.thanh@gmail.com>
2019-07-19 10:48:57 -0600

    Add jsx to initial character component

--- src/components/InitialCharacter/InitialCharacter.js
+++ src/components/InitialCharacter/InitialCharacter.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
 const InitialCharacter = () => {
   return (
-    <div>
-      
+    <div className='images'>
+      {/* <a data-tip data-for={name}>
+        <img className='car-imgs' src={img} />
+      </a>
+      <ReactTooltip id={name} type='dark' >
+          <span>{name}</span>
+      </ReactTooltip> */}
     </div>
   )
 }
