1a17d2bac4375f4376847e12a135d84fa3273135
raechel <odom.raechel@gmail.com>
2019-07-20 20:13:32 -0600

    app mdtp testing complete

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import {App, mapDispatchToProps } from './App';
+import { fetchChampions } from '../../thunks/fetchChampions';
+
+jest.mock('../../thunks/fetchChampions')
 
 describe('App', () => {
 
   let wrapper;
+  let mockFetchChampions = jest.fn();
 
   beforeEach(() => {
-    wrapper = shallow(<App/>);
+    wrapper = shallow(<App fetchChampions={mockFetchChampions} />);
+    
   });
 
   it('should match the snapshot with all the data passed in', () => {
     expect(wrapper).toMatchSnapshot();
-  })
+  });
+});
+
+describe('mapDispatchToProps', () => {
+ 
+  let mockDispatch = jest.fn();
+
+  it('should dispatch champions on the call of fetchChampions',() => {
+    const mockState = {
+      champions: [{name: 'Blitzcrank'}]
+    }
+    const actionToDispatch = fetchChampions(mockState);
+    const mappedProps = mapDispatchToProps(mockDispatch);
+    mappedProps.fetchChampions(mockState);
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  });
 });
