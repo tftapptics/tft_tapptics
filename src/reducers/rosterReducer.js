c2a0d5fc516dba4be5f84bb3d0d47d39b1d7605b
Duy Vu <duyv.thanh@gmail.com>
2019-07-23 16:26:59 -0600

    Correct index for roster displays correct hero

--- src/reducers/rosterReducer.js
+++ src/reducers/rosterReducer.js
@@ -1,11 +1,14 @@
-export const setRoster = (state = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}] , action) => {
+const dumbyData = {
+  attributes: {
+    model_img: ''
+  }
+}
+
+export const setRoster = (state = [dumbyData, dumbyData, dumbyData, dumbyData, dumbyData, dumbyData, dumbyData, dumbyData, dumbyData, dumbyData, dumbyData, dumbyData] , action) => {
   switch(action.type) {
     case 'SET_ROSTER':
       return action.champ
     case 'UPDATE_ROSTER':
-      console.log("hello world how are you doing")
-      console.log(action.index)
-      console.log(action.charInfo)
       return state.map((rosterPoint, idx) => {
         if(action.index === idx) {
           return action.charInfo
